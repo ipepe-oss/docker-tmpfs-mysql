4d18fd92e2dec14a544ce07374759583b0394868
Patryk Ptasinski <patryk@ipepe.pl>
2020-08-19 12:25:47 +0200

    Fix wrong tmpfs path

--- tmpfs-entrypoint.sh
+++ tmpfs-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 echo "Mounting TMPFS with size of ${MYSQLD_RAM_MB_SIZE}m"
 mkdir -p /mnt/ramdisk
-mount -t tmpfs -o size="${MYSQLD_RAM_MB_SIZE}m" tmpfs /var/lib/mysql/
+mount -t tmpfs -o size="${MYSQLD_RAM_MB_SIZE}m" tmpfs /mnt/ramdisk
 
 ./usr/local/bin/docker-entrypoint.sh "$@"
